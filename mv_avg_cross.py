5d36709a15792989e001b8fdc35cc878347e4f2c
skymere137 <kaisergw2@gmail.com>
2024-08-14 13:23:15 -0700

    Added functionality for a moving verage crossover strategy

--- mv_avg_cross.py
+++ mv_avg_cross.py
@@ -11,12 +11,26 @@ def get_dataframe(data):
 
     return data
 
-results = get_dataframe(r"data/ACHR.json")
+results = get_dataframe(r"data/SOUN.json")
 
+results["avg50"] = results["close"].rolling(50).mean()
+results["avg200"] = results["close"].rolling(200).mean()
 
+results["close1"] = results["close"].shift(-1)
 
+condition = results["avg50"] > results["avg200"]
 
+results["shares"] = [1 if condition.loc[ei] else 0 for ei in results.index]
 
+profit_condition = results["shares"] >= 0
 
+results["profit"] = [results.loc[ei, "close1"] - results.loc[ei, "close"] if results.loc[ei, "shares"] >= 1 else 0 for ei in results.index]
 
+results["wealth"] = results["profit"].cumsum()
 
+plt.title(f"Total money you have made is {results.loc[results.index[-2], 'wealth']}")
+plt.figure(figsize=(10, 8))
+results["avg50"].plot()
+results["avg200"].plot()
+
+plt.show()
